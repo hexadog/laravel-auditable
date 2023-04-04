ec8d9e32ff1f401bfd5254200017c8544419806b
Gaetan <72258504+gaetan-hexadog@users.noreply.github.com>
2023-04-04 13:45:37 +0200

    Revert "fix(provider): wrong blueprint macro callback signature"
    This reverts commit a943d81f21c2b6f4df5111b9789aa55ed9f3f5de.

--- src/Providers/EloquentServiceProvider.php
+++ src/Providers/EloquentServiceProvider.php
@@ -12,17 +12,17 @@ class EloquentServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blueprint::macro('auditable', function ($table) {
-            $table->unsignedBigInteger('created_by')->nullable()->index();
-            $table->timestamp('created_at')->nullable()->index();
-            $table->unsignedBigInteger('updated_by')->nullable()->index();
-            $table->timestamp('updated_at')->nullable()->index();
-            $table->unsignedBigInteger('deleted_by')->nullable()->index();
-            $table->timestamp('deleted_at')->nullable()->index();
+        Blueprint::macro('auditable', function () {
+            $this->unsignedBigInteger('created_by')->nullable()->index();
+            $this->timestamp('created_at')->nullable()->index();
+            $this->unsignedBigInteger('updated_by')->nullable()->index();
+            $this->timestamp('updated_at')->nullable()->index();
+            $this->unsignedBigInteger('deleted_by')->nullable()->index();
+            $this->timestamp('deleted_at')->nullable()->index();
         });
 
-        Blueprint::macro('dropAuditable', function ($table) {
-            $table->dropColumn(['created_by', 'created_at', 'updated_by', 'updated_at', 'deleted_by', 'deleted_at']);
+        Blueprint::macro('dropAuditable', function () {
+            $this->dropColumn(['created_by', 'created_at', 'updated_by', 'updated_at', 'deleted_by', 'deleted_at']);
         });
     }
 }
